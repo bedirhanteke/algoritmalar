5646fa14c4dc6e1f48ede58a902cd32e4cb37bc8
Bedirhan Teke <bedirhan.teke00@gmail.com>
2026-03-28 17:43:59 +0300

    MatrisKayanPencereToplamı sınıfı eklendi; matris üzerinde kayan pencere toplamını hesaplayan EnBuyukToplam metodu tanımlandı.

---
+++ src/MatrisKayanPencereToplamı.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace algoritmalar.src
+{
+    public class MatrisKayanPencereToplamı
+    {
+        public static int EnBuyukToplam(int[,]matris, int altBoyut)
+        {
+            int satirSayisi = matris.GetLength(0);
+            int sutunSayisi = matris.GetLength(1);
+            int maxToplam = int.MinValue;
+            //dış dögüler matris üzerinde pencereyi kaydırmak için yapılır 
+            for(int i=0; i <=satirSayisi-altBoyut; i++)
+            {
+                for(int j = 0; j<= sutunSayisi - altBoyut; j++)
+                {
+                    int mevcutToplam = 0;
+                    
+                    //matris içinde dolaşacak çerçeve oluşturulur 
+                    for(int x= i; x<i + altBoyut; x++) //satırı gezer 
+                    {
+                        for(int y=j; y< j + altBoyut; y++)
+                        {
+                            //çerçerçeve o an neredeyse mevcut değer içine toplamı biriktirir 
+                            mevcutToplam += matris[x,y];
+                        }
+                    }
+                }
+            }
+
+            return maxToplam;
+        }
+    }
+}
